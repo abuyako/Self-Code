4c6f1607818e227d03d7189a6aa4a8e84c63d5d4
abuyako <abuyako@gmail.com>
2019-12-16 15:20:08 +0300

    Fixing the syntax error

--- includes/main-body.php
+++ includes/main-body.php
@@ -5,28 +5,28 @@
             <h1>JavaScript</h1>
             <p>This is the scripting language</p>
             </div>
-            <img src="./asset/images/js.png" alt="">
+            <img src="./assets/images/js.png" alt="">
         </div>
         <div class="card card2">
         <div class="card2-text">
             <h1>PHP</h1>
             <p>This is the server-side language</p>
             </div>
-            <img src="./asset/images/php.png" alt="">
+            <img src="./assets/images/php.png" alt="">
         </div>
         <div class="card card3">
         <div class="card3-text">
             <h1>HTML</h1>
             <p>This is the Hyper-texting markup language</p>
             </div>
-            <img src="./asset/images/html.png" alt="">
+            <img src="assets/images/html.png" alt="">
         </div>
         <div class="card card4">
         <div class="card4-text">
             <h1>CSS</h1>
             <p>This is the styling language</p>
             </div>
-            <img src="./asset/images/css.png" alt="">
+            <img src="./assets/images/css.png" alt="">
         </div>
     </div>
 </section>
