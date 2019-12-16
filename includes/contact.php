26733b1ca1a9bcef5ed59d43ef3710cee9d48c0b
abuyako <abuyako@gmail.com>
2019-12-16 16:30:52 +0300

    Adding the forms in the contact

---
+++ includes/contact.php
@@ -0,0 +1,13 @@
+<div class="contact">
+    <img src="" alt="">
+    <div class="contact-form">
+        <form action="">
+            <label for="name">name</label>
+            <input type="text" name="name" placeholder="enter you name">
+            <label for="email"></label>
+            <input type="text" name="email" id="" placeholder="enter you email">
+            <label for="comments"></label>
+            <input type="textarea" min="10px" max="50px" placeholder="Comment Here...">
+        </form>
+    </div>
+</div>
