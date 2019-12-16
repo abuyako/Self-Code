3cbdc68d8e98625cbe083c0577ed12fe31b014a6
abuyako <abuyako@gmail.com>
2019-12-16 16:54:34 +0300

    Adding the Logo for the website

--- includes/heading.php
+++ includes/heading.php
@@ -1,4 +1,7 @@
 <div class="heading">
+        <div class="heading-logo">
+            <img src="./assets/images/logo.png" alt="">
+        </div>
         <div class="heading-text">
             <h1>Self Code</h1>
             <p>Today I'am writtng code for myself</p>
