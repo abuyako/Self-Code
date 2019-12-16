1c06ea3b4c2d6b4a6ec59beedb7a81031e87276c
abuyako <abuyako@gmail.com>
2019-12-16 15:07:19 +0300

    Adding the main-body

---
+++ main-body.php
@@ -0,0 +1,32 @@
+<section class="main-body">
+    <div class="main-body-cards">
+        <div class="card card1">
+            <div class="card1-text">
+            <h1>JavaScript</h1>
+            <p>This is the scripting language</p>
+            </div>
+            <img src="./asset/images/js.png" alt="">
+        </div>
+        <div class="card card2">
+        <div class="card2-text">
+            <h1>PHP</h1>
+            <p>This is the server-side language</p>
+            </div>
+            <img src="./asset/images/php.png" alt="">
+        </div>
+        <div class="card card3">
+        <div class="card3-text">
+            <h1>HTML</h1>
+            <p>This is the Hyper-texting markup language</p>
+            </div>
+            <img src="./asset/images/html.png" alt="">
+        </div>
+        <div class="card card4">
+        <div class="card4-text">
+            <h1>CSS</h1>
+            <p>This is the styling language</p>
+            </div>
+            <img src="./asset/images/css.png" alt="">
+        </div>
+    </div>
+</section>
