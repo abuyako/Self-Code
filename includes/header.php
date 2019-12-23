25b416213494c22efd675e5fd8efcecd8df2895b
abuyako <abuyako@gmail.com>
2019-12-23 09:34:53 +0300

    Adding the font-awsome for icons

--- includes/header.php
+++ includes/header.php
@@ -5,4 +5,5 @@
     <title>Self Coding</title>
 
     <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
