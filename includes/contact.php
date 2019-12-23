2944953373240d46431efbe2d605608858fb8dea
abuyako <abuyako@gmail.com>
2019-12-23 09:05:06 +0300

    Adding the requared function on the forms

--- includes/contact.php
+++ includes/contact.php
@@ -3,9 +3,9 @@
     <div class="contact-forms">
         <form action="">
             <label for="name">Name</label> <br>
-            <input type="text" name="name" placeholder="name"> <br>
+            <input type="text" name="name" placeholder="name" required> <br>
             <label for="email">Email</label> <br>
-            <input type="text" name="email" id="" placeholder="email"> <br>
+            <input type="text" name="email" id="" placeholder="email" required> <br>
             <label for="comments">Comment</label> <br>
             <textarea name="comments" id="" cols="60" rows="15" placeholder="comment here..." ></textarea>
             <input  type="submit" value="submit">
