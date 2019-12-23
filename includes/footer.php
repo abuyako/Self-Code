9d92a921efa8d34d3576681e24154e8e3256c2f0
abuyako <abuyako@gmail.com>
2019-12-23 09:55:09 +0300

    Adding other social icons

--- includes/footer.php
+++ includes/footer.php
@@ -8,6 +8,10 @@
         </ul>
     </div>
     <div class="page-footer-social">
+         <i class="fa fa-intagram-square" style="color:white; font-size: 30px"></i>
          <i class="fa fa-facebook-square" style="color:white; font-size: 30px"></i>
+         <i class="fa fa-linkedin-square" style="color:white; font-size: 30px"></i>   
+         <i class="fa fa-twitter-square" style="color:white; font-size: 30px"></i>      
+         <i class="fa fa-github-square" style="color:white; font-size: 30px"></i>
     </div>
 </div>
