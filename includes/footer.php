bfb6d9ee95738e0c279777134dec2746ccb683c0
abuyako <abuyako@gmail.com>
2019-12-23 09:34:08 +0300

    Adding the list of items in the footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,11 @@
 <div class="page-footer">
-    <div class="page-footer-list"></div>
+    <div class="page-footer-list">
+        <ul>
+            <li>self Coding</li>
+            <li>abuyako@gmail.com</li>
+            <li>Ubungo - Dar Es Salaam</li>
+            <li>+255752080410</li>
+        </ul>
+    </div>
     <div class="page-footer-social"></div>
 </div>
