c33e731595a253b5eaec2660283c2012130bbfea
abuyako <abuyako@gmail.com>
2020-01-06 22:24:42 +0300

    Adding the inputs in the forms

--- includes/signup-form.php
+++ includes/signup-form.php
@@ -2,7 +2,10 @@
     <div class="signup-forms">
         <h1>Sign Up</h1>
         <form action="">
-            <input type="text" name="username" placeholder="username or email" required> <br>
+            <input type="text" name="username" placeholder="first name" required> <br>
+            <input type="text" name="username" placeholder="second name" required> <br>
+            <input type="text" name="username" placeholder="username" required> <br>
+            <input type="text" name="username" placeholder="email" required> <br>
             <input type="text" name="password" placeholder="password" required><br>
             <input type="submit" name="submit" value="SIGN IN">
         </form>
