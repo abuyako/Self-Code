27550b91371f32ff837c4f612d3dde8da7383223
abuyako <abuyako@gmail.com>
2020-01-06 22:06:25 +0300

    Adding the forms in the signup forms

---
+++ signup.php
@@ -0,0 +1,12 @@
+<div class="signup">
+    <div class="signup-forms">
+        <h1>Sign Up</h1>
+        <form action="">
+            <input type="text" name="username" placeholder="username or email" required> <br>
+            <input type="text" name="password" placeholder="password" required><br>
+            <input type="submit" name="submit" value="SIGN IN">
+        </form>
+        <p>Create Account</p>
+        <a href="Signup.php">SIGN UP</a>
+    </div>
+</div>
