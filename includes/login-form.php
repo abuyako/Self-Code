4294a16befaa2428c45ad3af0d36109dff9f5e04
abuyako <abuyako@gmail.com>
2020-01-06 11:05:55 +0300

    Adding the forms input in the login form

--- includes/login-form.php
+++ includes/login-form.php
@@ -1,5 +1,10 @@
 <div class="login">
     <div class="login-forms">
         <h1>Sign In</h1>
+        <form action="">
+            <input type="text" name="username" placeholder="username or email"> <br>
+            <input type="text" name="password" placeholder="password"><br>
+            <input type="submit" name="submit" value="SIGN IN">
+        </form>
     </div>
 </div>
