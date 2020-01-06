bfc4425d55434a0cd74542f637c6a38d0b5fed0c
abuyako <abuyako@gmail.com>
2020-01-06 10:18:06 +0300

    Modified the login.php to index.php

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-     
-     <?php include_once("includes/header.php");?>
+
+    <?php include_once("includes/header.php");?>
+
 <body>
     
-    <?php include_once("includes/heading.php");?>
-    <?php include_once("includes/main-body.php")?>
-    <?php include_once("includes/contact.php")?>
-    <?php include_once("includes/footer.php");?>
+ 
 
 </body>
 </html>
