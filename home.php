c0e9d53a586ce4a8002af026441b509545fdca46
abuyako <abuyako@gmail.com>
2020-01-06 10:16:46 +0300

    Changing the index.php to home.php

---
+++ home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+     
+     <?php include_once("includes/header.php");?>
+<body>
+    
+    <?php include_once("includes/heading.php");?>
+    <?php include_once("includes/main-body.php")?>
+    <?php include_once("includes/contact.php")?>
+    <?php include_once("includes/footer.php");?>
+
+</body>
+</html>
