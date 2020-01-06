888df54af60e19e90ef506a1ac7b76996fa170f9
abuyako <abuyako@gmail.com>
2020-01-06 12:02:39 +0300

    Adding the sign up button

--- includes/login-form.php
+++ includes/login-form.php
@@ -2,9 +2,11 @@
     <div class="login-forms">
         <h1>Sign In</h1>
         <form action="">
-            <input type="text" name="username" placeholder="username or email"> <br>
-            <input type="text" name="password" placeholder="password"><br>
+            <input type="text" name="username" placeholder="username or email" required> <br>
+            <input type="text" name="password" placeholder="password" required><br>
             <input type="submit" name="submit" value="SIGN IN">
         </form>
+        <p>Create Account</p>
+        <input type="submit" name="submit" value="SIGN UP">
     </div>
 </div>
