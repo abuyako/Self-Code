be5838cd5eac758bb790a521a5c539626df0ae0f
abuyako <abuyako@gmail.com>
2020-01-07 17:18:44 +0300

    Fixing the row size of the textarea

--- includes/contact.php
+++ includes/contact.php
@@ -7,7 +7,7 @@
             <label for="email">Email</label> <br>
             <input type="text" name="email" id="" placeholder="email" required> <br>
             <label for="comments">Comment</label> <br>
-            <textarea name="comments" id="" cols="60" rows="15" placeholder="comment here..." ></textarea>
+            <textarea name="comments" id="" cols="60" rows="10" placeholder="comment here..." ></textarea>
             <input class="contact-button" type="submit" value="submit">
         </form>
     </div>
