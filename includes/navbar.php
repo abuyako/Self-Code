d3b3b0d2813374d32d102c1b045da44c6dc0ff79
abuyako <abuyako@gmail.com>
2020-01-08 11:18:00 +0300

    Adding the list in the navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,1 +1,10 @@
-<div class="navbar"></div>
+<div class="navbar">
+    <div class="navabar-list">
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">CONTACT</a></li>
+            <li><a href="#">LOCATION</a></li>
+        </ul>
+    </div>
+</div>
