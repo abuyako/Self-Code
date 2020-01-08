50390f8cb651750fea7a7ec1ce5cb8500e1b5229
abuyako <abuyako@gmail.com>
2020-01-08 11:12:03 +0300

    Adding the navbar.php in the home page

--- home.php
+++ home.php
@@ -5,6 +5,7 @@
 <body>
     
     <?php include_once("includes/heading.php");?>
+    <?php include_once("includes/navbar.php");?>
     <?php include_once("includes/main-body.php")?>
     <?php include_once("includes/contact.php")?>
     <?php include_once("includes/footer.php");?>
