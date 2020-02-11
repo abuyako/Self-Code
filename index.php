daae93094e11067967e278a36b6c27d94337ff9c
abuyako <abuyako@gmail.com>
2020-02-12 00:55:00 +0300

    modified files

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
-    <?php include_once("includes/header.php");?>
-
+     
+     <?php include_once("includes/header.php");?>
 <body>
     
     <?php include_once("includes/heading.php");?>
-    <?php include_once("includes/login-form.php");?>
+    <?php include_once("includes/navbar.php");?>
+    <?php include_once("includes/main-body.php")?>
+    <?php include_once("includes/about.php")?>
+    <?php include_once("includes/contact.php")?>
     <?php include_once("includes/footer.php");?>
 
 </body>
