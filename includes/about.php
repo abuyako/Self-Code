4c8a66cb9725ab19cd4badddf25250e6743e136d
abuyako <abuyako@gmail.com>
2020-02-12 00:31:44 +0300

    adding the about text and styling them

--- includes/about.php
+++ includes/about.php
@@ -1,3 +1,9 @@
 <div class="about">
-    
+    <div class="about-text">
+        <h1>What we do...</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, 
+            dolorum eaque, veniam ipsa perferendis ipsam odio velit molestiae 
+            cupiditate inventore nemo libero labore! Quae deleniti, quia ipsa cum 
+            aspernatur dignissimos.</p>
+    </div>
 </div>
