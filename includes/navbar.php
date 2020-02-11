4b0c700b3de9bed44ab94cf8aee7dd036bdc36a5
abuyako <abuyako@gmail.com>
2020-02-11 23:27:31 +0300

    styling the navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,3 +1,9 @@
 <div class="navbar">
-    <div class="navbar-list"></div>
+    <div class="navbar-list">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+        </ul>
+    </div>
 </div>
