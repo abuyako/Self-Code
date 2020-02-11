f8b56ebc523d713b36fa401483e9cad0d4cc564b
abuyako <abuyako@gmail.com>
2020-02-11 23:46:22 +0300

    adding nav-list and styling

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,8 +2,10 @@
     <div class="navbar-list">
         <ul>
             <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Location</a></li>
+            <li><a href="#">FAQ</a></li>
         </ul>
     </div>
 </div>
