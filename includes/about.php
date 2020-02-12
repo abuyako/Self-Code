928822cf7e51ebbc9034a2eef4c1d316f8d1c14f
abuyako <abuyako@gmail.com>
2020-02-13 00:12:11 +0300

    styling cards

--- includes/about.php
+++ includes/about.php
@@ -1,10 +1,7 @@
 <div class="about">
     <div class="about-text">
         <h1>What we do...</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, 
-            dolorum eaque, veniam ipsa perferendis ipsam odio velit molestiae 
-            cupiditate inventore nemo libero labore! Quae deleniti, quia ipsa cum 
-            aspernatur dignissimos.</p>
+        <p>Lorem ipsum dolor sit amet consectetur </p>
     </div>
 
   <div class="about-card">
