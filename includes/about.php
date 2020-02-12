f2efd37a2fc2ad15f6cc740093304ca4bf51eeee
abuyako <abuyako@gmail.com>
2020-02-13 00:37:32 +0300

    styling the background image linier gradient

--- includes/about.php
+++ includes/about.php
@@ -1,15 +1,15 @@
 <div class="about">
     <div class="about-text">
-        <h1>What we do...</h1>
+        <h1>What we do.</h1>
         <p>Lorem ipsum dolor sit amet consectetur </p>
     </div>
 
   <div class="about-card">
-    <div class="card-1"></div>
-    <div class="card-1"></div>
-    <div class="card-1"></div>
-    <div class="card-1"></div>
-    <div class="card-1"></div>
-    <div class="card-1"></div>
+    <div class="card-1 card-01"></div>
+    <div class="card-1 card-02"></div>
+    <div class="card-1 card-03"></div>
+    <div class="card-1 card-04"></div>
+    <div class="card-1 card-05"></div>
+    <div class="card-1 card-06"></div>
   </div>
 </div>
