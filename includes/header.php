416c121f08fd5fe3be9f80b03da0b1f191b390f4
abuyako <abuyako@gmail.com>
2020-02-14 16:23:41 +0300

    adding font link for cdnjs

--- includes/header.php
+++ includes/header.php
@@ -8,4 +8,5 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="animate.min.css">
     <link href="https://fonts.googleapis.com/css?family=Amaranth:400,400i,700,700i&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
 </head>
