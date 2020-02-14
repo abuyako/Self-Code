26a86bef514223e4dc7e1cf3ec41664aeef46b64
abuyako <abuyako@gmail.com>
2020-02-14 15:51:43 +0300

    adding amarath fonts

--- includes/header.php
+++ includes/header.php
@@ -7,4 +7,5 @@
     <link rel="stylesheet" href="assets/css/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="animate.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Amaranth:400,400i,700,700i&display=swap" rel="stylesheet">
 </head>
