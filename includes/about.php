653716ac4965a3b02771369847898343b5f06e45
abuyako <abuyako@gmail.com>
2020-02-14 18:49:38 +0300

    Adding want we do tasks

--- includes/about.php
+++ includes/about.php
@@ -5,11 +5,11 @@
     </div>
 
   <div class="about-card">
-    <div class="card-1 card-01"></div>
-    <div class="card-1 card-02" style="color:white; text-align:center; font-zise:50px">On Progress</div>
-    <div class="card-1 card-03"></div>
-    <div class="card-1 card-04"></div>
-    <div class="card-1 card-05"></div>
-    <div class="card-1 card-06"></div>
+    <div class="card-1 card-01" style="color:white; text-align:center; font-zise:50px">Self Teach</div>
+    <div class="card-1 card-02" style="color:white; text-align:center; font-zise:50px">Snipet</div>
+    <div class="card-1 card-03" style="color:white; text-align:center; font-zise:50px">Tips & Tricks</div>
+    <div class="card-1 card-04" style="color:white; text-align:center; font-zise:50px">Code Editor</div>
+    <div class="card-1 card-05" style="color:white; text-align:center; font-zise:50px">UI/UX Designs</div>
+    <div class="card-1 card-06" style="color:white; text-align:center; font-zise:50px">Frameworks</div>
   </div>
 </div>
