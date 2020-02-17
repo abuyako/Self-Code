13013d614c213a5932abc49851e3b89569e05a0a
abuyako <abuyako@gmail.com>
2020-02-17 18:43:18 +0300

    linking the location in a navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,7 +4,7 @@
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Contact</a></li>
-            <li><a href="#">Location</a></li>
+            <li><a href="location.php">Location</a></li>
             <li><a href="#">FAQ</a></li>
             <li><a href="signin.php">SignIn</a></li>
             <li><a href="signup.php">SignUp</a></li>
