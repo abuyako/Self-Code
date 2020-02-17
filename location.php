71356e1088d1822445404be69e2e5503261300e3
abuyako <abuyako@gmail.com>
2020-02-17 18:42:02 +0300

    adding header,footer and heading in a location page

---
+++ location.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php include_once("includes/header.php");?>
+
+<body>
+    
+    <?php include_once("includes/heading.php");?>
+
+    <?php include_once("includes/footer.php");?>
+</body>
+</html>
