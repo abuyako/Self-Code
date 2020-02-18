366985605f8f9949f574d96ffca2fdad48a7a6f4
abuyako <abuyako@gmail.com>
2020-02-18 11:45:31 +0300

    embeded google map API on a location

--- includes/location-page.php
+++ includes/location-page.php
@@ -1,5 +1,12 @@
 <div class="location-page">
-    <div class="location-text">
-        <h1>Find Us Here</h1>
-    </div>
+    <div class="location-text"><h1>Find Us Here</h1></div>
+    <div class="mapouter">
+    <div class="gmap_canvas"><iframe width="634" height="530" id="gmap_canvas"
+     src="https://maps.google.com/maps?q=Dar%20es%20salaam%2Cmakumbusho&t=&z=13&ie=UTF8&iwloc=&output=embed" 
+     frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/nordvpn-coupon-code/">go to the nord vpn website</a>
+     </div>
+     <style>.mapouter{text-align:right;height:530px;width:650px;margin: 0 auto;}.gmap_canvas
+     {overflow:hidden;background:none!important;height:530px;width:634px;}</style>
+     
+     </div>
 </div>
