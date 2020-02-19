bbf8e2439b686baa821c29b860505eb2390e1bce
abuyako <abuyako@gmail.com>
2020-02-19 11:23:51 +0300

    adding styling on about section

--- includes/about.php
+++ includes/about.php
@@ -8,8 +8,8 @@
     <div class="card-1 card-01" style="color:white; text-align:center; font-zise:50px">Self Teach</div>
     <div class="card-1 card-02" style="color:white; text-align:center; font-zise:50px">Snipet</div>
     <div class="card-1 card-03" style="color:white; text-align:center; font-zise:50px">Tips & Tricks</div>
-    <div class="card-1 card-04" style="color:white; text-align:center; font-zise:50px">Code Editor</div>
-    <div class="card-1 card-05" style="color:white; text-align:center; font-zise:50px">UI/UX Designs</div>
-    <div class="card-1 card-06" style="color:white; text-align:center; font-zise:50px">Frameworks</div>
+    <div class="card-1 card-04 card-00" style="color:white; text-align:center; font-zise:50px">Code Editor</div>
+    <div class="card-1 card-05 card-00" style="color:white; text-align:center; font-zise:50px">UI/UX Designs</div>
+    <div class="card-1 card-06 card-00" style="color:white; text-align:center; font-zise:50px">Frameworks</div>
   </div>
 </div>
