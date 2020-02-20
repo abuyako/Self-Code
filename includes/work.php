4af329a1f4af6b937186f52d793741b59992aa91
abuyako <abuyako@gmail.com>
2020-02-20 14:48:53 +0300

    adding heading on work

--- includes/work.php
+++ includes/work.php
@@ -1,1 +1,5 @@
-<div class="work"></div>
+<div class="work">
+    <h1>Let's get to work.</h1>
+    <h2>Hava an unsolved problem or audacious idea?</h2>
+    <p>We'd love to hear about it.</p>
+</div>
