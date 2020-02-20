abcf4b1371985f735d5c3b2c62fd680acaab9b3b
abuyako <abuyako@gmail.com>
2020-02-20 18:28:17 +0300

    Modifiying color in the site

--- includes/footer.php
+++ includes/footer.php
@@ -12,10 +12,10 @@
         <p>"Talk is cheap, Show me the code"</p>
     </div>
     <div class="page-footer-social">
-         <a href="https://www.instagram.com/itsabuyako"><i class="fa fa-instagram" style="color:white; font-size: 30px"></i></a>
-         <a href="https://web.facebook.com/abubakary.kowero"><i class="fa fa-facebook-square" style="color:white; font-size: 30px"></i></a>
-         <a href="https://www.linkedin.com/in/abubakary-kowero-6a8ba0104/"><i class="fa fa-linkedin-square" style="color:white; font-size: 30px"></i></a>
-         <a href="https://twitter.com/abuyako"><i class="fa fa-twitter-square" style="color:white; font-size: 30px"></i> </a>     
-         <a href="https://github.com/abuyako"><i class="fa fa-github-square" style="color:white; font-size: 30px"></i></a>
+        <a href="https://www.instagram.com/itsabuyako"><i class="fa fa-instagram" style="color:bisque; font-size: 30px"></i></a>
+         <a href="https://web.facebook.com/abubakary.kowero"><i class="fa fa-facebook-square" style="color:bisque; font-size: 30px"></i></a>
+         <a href="https://www.linkedin.com/in/abubakary-kowero-6a8ba0104/"><i class="fa fa-linkedin-square" style="color:bisque; font-size: 30px"></i></a>
+         <a href="https://twitter.com/abuyako"><i class="fa fa-twitter-square" style="color:bisque; font-size: 30px"></i> </a>     
+         <a href="https://github.com/abuyako"><i class="fa fa-github-square" style="color:bisque; font-size: 30px"></i></a>
     </div>
 </div>
