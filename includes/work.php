2f539c47411c9600e3b8c733a43738800cbfa41f
abuyako <abuyako@gmail.com>
2020-02-20 14:50:05 +0300

    adding button

--- includes/work.php
+++ includes/work.php
@@ -2,4 +2,6 @@
     <h1>Let's get to work.</h1>
     <h2>Hava an unsolved problem or audacious idea?</h2>
     <p>We'd love to hear about it.</p>
+
+    <button class="work-btn">CONTANCT US</button>
 </div>
