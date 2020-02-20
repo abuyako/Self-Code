53c689a9b03ba740e55116e5c7d78078da271d56
abuyako <abuyako@gmail.com>
2020-02-20 17:47:31 +0300

    adding image logo

--- includes/heading.php
+++ includes/heading.php
@@ -1,5 +1,6 @@
 <div class="heading">
         <div class="heading-logo">
+            <img src="../assets/images/loga.png" alt="logo">
         </div>
         <div class="heading-text">
             <h1>Self Code</h1>
