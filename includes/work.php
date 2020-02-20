462a192a92b1d33459fa326d01db7186050386fd
abuyako <abuyako@gmail.com>
2020-02-20 15:05:37 +0300

    styling the heading in work

--- includes/work.php
+++ includes/work.php
@@ -1,7 +1,9 @@
 <div class="work">
-    <h1>Let's get to work.</h1>
-    <h2>Hava an unsolved problem or audacious idea?</h2>
-    <p>We'd love to hear about it.</p>
+    <div class="work-text">
+        <h1>Let's get to work.</h1>
+        <h2>Have an unsolved problem or audacious idea?</h2>
+        <p>We'd love to hear about it.</p>
+    </div>
 
     <button class="work-btn">CONTANCT US</button>
 </div>
