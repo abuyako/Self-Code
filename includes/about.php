04c1a0ec27dee6fb7b6ec1b54db988448a019c96
abuyako <abuyako@gmail.com>
2020-02-24 17:23:36 +0300

    Styling card-01

--- includes/about.php
+++ includes/about.php
@@ -6,16 +6,22 @@
     </div>
 
   <div class="about-card">
+
     <div class="card-1 card-01" style="color:bisque; text-align:center; font-zise:50px">
     <!-- Self Teach -->
     <div class="card-01-picture">
       <!-- <img src="../assets/images/self-taught.png" alt="" style="width:100%;"> -->
-      
     </div>
     <div class="card-01-text">
-      <h1>this is where you learn for yourself</h1>
+      <h1>SELF TAUGHT</h1>
+      <p>Online Leaning is the one of the best ways of self taught</p>
+
+     <div class="anchor">
+     <a href="#">Read More</a>
+     </div>
     </div>
     </div>
+
     <div class="card-1 card-02" style="color:bisque; text-align:center; font-zise:50px">Snipet</div>
     <div class="card-1 card-03" style="color:bisque; text-align:center; font-zise:50px">Tips & Tricks</div>
     <div class="card-1 card-04 card-00" style="color:bisque; text-align:center; font-zise:50px">Books</div>
