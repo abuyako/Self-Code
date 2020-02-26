2746bae9ec019c612dbf16912d7b2b09e9eb7a88
abuyako <abuyako@gmail.com>
2020-02-26 11:13:40 +0300

    removing the width

--- includes/about.php
+++ includes/about.php
@@ -13,16 +13,28 @@
       <!-- <img src="../assets/images/self-taught.png" alt="" style="width:100%;"> -->
     </div>
     <div class="card-01-text">
-      <h1>SELF TAUGHT</h1>
-      <p>Online Leaning is the one of the best ways of self taught</p>
+        <h1>SELF TAUGHT</h1>
+       <p>Online Leaning is the one of the best ways of self taught</p>
 
      <div class="anchor">
-     <a href="#">Read More</a>
+        <a href="#">Read More</a>
      </div>
     </div>
     </div>
 
-    <div class="card-1 card-02" style="color:bisque; text-align:center; font-zise:50px">Snipet</div>
+    <div class="card-1 card-02" style="color:bisque; text-align:center; font-zise:50px">
+    <div class="card-02-picture">
+      <!-- <img src="../assets/images/self-taught.png" alt="" style="width:100%;"> -->
+    </div>
+    <div class="card-02-text">
+        <h1>SNIPET</h1>
+       <p>Here you will get different lines of code simple and smart</p>
+
+     <div class="anchor">
+        <a href="#">Read More</a>
+     </div>
+    </div>
+    </div>
     <div class="card-1 card-03" style="color:bisque; text-align:center; font-zise:50px">Tips & Tricks</div>
     <div class="card-1 card-04 card-00" style="color:bisque; text-align:center; font-zise:50px">Books</div>
     <!-- <div class="card-1 card-05 card-00" style="color:white; text-align:center; font-zise:50px">UI/UX Designs</div>
