e02aaa4dd42405ab9f0f1fc9f409e721e95a26ab
abuyako <abuyako@gmail.com>
2020-02-26 12:23:00 +0300

    styling card-02

--- includes/about.php
+++ includes/about.php
@@ -7,10 +7,9 @@
 
   <div class="about-card">
 
+  <!-- card-01 -->
     <div class="card-1 card-01" style="color:bisque; text-align:center; font-zise:50px">
-    <!-- Self Teach -->
     <div class="card-01-picture">
-      <!-- <img src="../assets/images/self-taught.png" alt="" style="width:100%;"> -->
     </div>
     <div class="card-01-text">
         <h1>SELF TAUGHT</h1>
@@ -22,19 +21,21 @@
     </div>
     </div>
 
+  <!-- card-02 -->
     <div class="card-1 card-02" style="color:bisque; text-align:center; font-zise:50px">
     <div class="card-02-picture">
-      <!-- <img src="../assets/images/self-taught.png" alt="" style="width:100%;"> -->
     </div>
     <div class="card-02-text">
-        <h1>SNIPET</h1>
-       <p>Here you will get different lines of code simple and smart</p>
+        <h1>SELF TAUGHT</h1>
+        <p>Online Leaning is the one of the best ways of self taught</p>
 
      <div class="anchor">
         <a href="#">Read More</a>
      </div>
     </div>
     </div>
+
+    <!-- card-03 -->
     <div class="card-1 card-03" style="color:bisque; text-align:center; font-zise:50px">Tips & Tricks</div>
     <div class="card-1 card-04 card-00" style="color:bisque; text-align:center; font-zise:50px">Books</div>
     <!-- <div class="card-1 card-05 card-00" style="color:white; text-align:center; font-zise:50px">UI/UX Designs</div>
